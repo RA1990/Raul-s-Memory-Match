56562324b1870fef1d1c188a0a42aa17a6b01a1a
raul jauregui <felx90@outlook.com>
2019-07-03 15:31:40 -0700

    still working on match

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
-$(document).ready(initailizeApp);
+$(document).ready(initializeApp);
+var firstCardClicked=null;
+var secondCardClicked=null;
+var matches=null;
 
-
-function initailizeApp(){
+function initializeApp(){
   $(".back").on("click",handleCardClkick);
 }
 function handleCardClkick(event){
   $(event.currentTarget).addClass("hide");
-  console.log(event.currentTarget);
+  console.log($this).find('background-image')
+  var currentCard = event.delegateTarget.parentElement.childNodes[1];
+  if(firstCardClicked === null){
+    firstCardClicked = $(currentCard);
+  }
+  if(firstCardClicked !== null){
+    secondCardClicked = $(currentCard);
+  }
+
+  console.log(event);
+  console.log(event.delegateTarget.parentElement.childNodes[1]);
+  console.log(event.currentTarget.baseURI);
 }
