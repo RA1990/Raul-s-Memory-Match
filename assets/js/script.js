8bf9ae1bcbdf13ad947c38bd81e2af7301293528
raul jauregui <felx90@outlook.com>
2019-07-03 10:26:16 -0700

    one card flip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("hrrthrth")
+$(document).ready(initailizeApp);
+
+
+function initailizeApp(){
+  $(".back").on("click",handleCardClkick);
+}
+function handleCardClkick(event){
+  $(event.currentTarget).addClass("hide");
+  console.log(event.currentTarget);
+}
