a88fdae7be9712688c71c3016bec875f6fa382ea
raul jauregui <felx90@outlook.com>
2019-07-03 17:28:45 -0700

    non matching cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,19 +2,29 @@ $(document).ready(initializeApp);
 var firstCardClicked=null;
 var secondCardClicked=null;
 var matches=null;
+var firstCardImage = null;
+var secondCardImage= null;
 
 function initializeApp(){
   $(".back").on("click",handleCardClkick);
 }
 function handleCardClkick(event){
   $(event.currentTarget).addClass("hide");
-  console.log($this).find('background-image')
   var currentCard = event.delegateTarget.parentElement.childNodes[1];
   if(firstCardClicked === null){
-    firstCardClicked = $(currentCard);
+    firstCardClicked = $(event.currentTarget)
+    firstCardImage = $(currentCard).css("background-image");
   }
   if(firstCardClicked !== null){
-    secondCardClicked = $(currentCard);
+    secondCardClicked = $(event.currentTarget);
+    secondCardImage = $(currentCard).css("background-image");
+  }
+  if(firstCardImage === secondCardImage){
+    matches +=1
+  }
+  if(firstCardImage !== secondCardImage){
+    setTimeout(function(){$(firstCardClicked).removeClass("hide")},2000);
+    setTimeout(function(){$(secondCardClicked).removeClass("hide")},2000);
   }
 
   console.log(event);
